f3c790fab16700652054c639d99b344b63089f49
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-04-04 17:19:02 -0300

    Pages system

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,71 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  PageController _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController();
+  }
+
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Theme.of(context).accentColor,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+            caption: TextStyle(color: Colors.white54)
+          )
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _page,
+          onTap: (p) {
+            _pageController.animateToPage(p, duration: Duration(milliseconds: 500), curve: Curves.ease);
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Clientes")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text("Pedidos")
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                title: Text("Produtos")
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: [
+          Container(color: Colors.red,),
+          Container(color: Colors.green,),
+          Container(color: Colors.blue,),
+        ],
+      ),
+    );
   }
 }
